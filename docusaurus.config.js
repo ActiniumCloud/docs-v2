c0cd8b6991fbaf649a08569f7b2b65014fb8d65a
RooRay <86845749+RooRay@users.noreply.github.com>
2022-09-25 00:46:07 +0100

    Fix broken links
    Fixes the broken links referring to the blog preventing docusaurus from building

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,6 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/ActiniumCloud/docs-v2',
             label: 'GitHub',
